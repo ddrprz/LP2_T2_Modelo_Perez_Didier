e7aef2d12b746611e07e8b9ab52ddfd4fd62c456
Didier Pérez <157639492+ddrprz@users.noreply.github.com>
2025-06-16 18:27:06 -0500

    Primer Commit

---
+++ src/main/java/com/example/demo/Lp2T2ModeloPerezDidierApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2T2ModeloPerezDidierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2T2ModeloPerezDidierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/Lp2T2ModeloPerezDidierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2T2ModeloPerezDidierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
